66c96f2861c0418dc800fbd7bc8a2f6ec5c838d2
AA55h <janprokupek@centrum.cz>
2025-02-26 10:41:40 +0100

    Create debug.sh

---
+++ scripts/debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -f Makefile ]; then
+    echo "Error: Makefile not found! Make sure you're executing from the root of the project!"
+    exit 1
+fi
+
+make clean-all && make debug
+gdb ./examples/main
